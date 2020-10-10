1105b49996b2cc7b6f8ccef6827c992e50be79a9
qkrdud8 <68595923+qkrdud8@users.noreply.github.com>
2020-10-10 19:55:47 +0900

    Add files via upload

---
+++ folder_management/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace folder_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.openfolder = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.addfolder = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(609, 382);
+            this.treeView1.TabIndex = 0;
+            // 
+            // openfolder
+            // 
+            this.openfolder.Location = new System.Drawing.Point(638, 12);
+            this.openfolder.Name = "openfolder";
+            this.openfolder.Size = new System.Drawing.Size(75, 23);
+            this.openfolder.TabIndex = 1;
+            this.openfolder.Text = "폴더열기";
+            this.openfolder.UseVisualStyleBackColor = true;
+            this.openfolder.Click += new System.EventHandler(this.openfolder_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(638, 51);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 2;
+            this.delete.Text = "삭제";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // addfolder
+            // 
+            this.addfolder.Location = new System.Drawing.Point(546, 413);
+            this.addfolder.Name = "addfolder";
+            this.addfolder.Size = new System.Drawing.Size(75, 23);
+            this.addfolder.TabIndex = 3;
+            this.addfolder.Text = "폴더 추가";
+            this.addfolder.UseVisualStyleBackColor = true;
+            this.addfolder.Click += new System.EventHandler(this.addfolder_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(512, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addfolder);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.openfolder);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button openfolder;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button addfolder;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ folder_management/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folder_management
+{
+    public partial class Form1 : Form
+    {
+        string ppath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        public void openfolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+            treeView1.Nodes.Add(Makenode(fbd.SelectedPath));
+            ppath = fbd.SelectedPath;
+        }
+
+
+
+        public TreeNode Makenode(string path)
+        {
+            TreeNode output = new TreeNode();
+            DirectoryInfo di = new DirectoryInfo(path);
+            output.Tag = path;
+            output.Text = Path.GetFileName((string)output.Tag);
+            
+
+            if (di.Attributes == FileAttributes.Directory)
+            {
+                foreach (string d in Directory.GetDirectories(path))
+                {
+                    output.Nodes.Add(Makenode(d));
+                }
+                foreach (string f in Directory.GetFiles(path))
+                {
+                    output.Nodes.Add(Makenode(f));
+                }
+                
+            }
+            return output;            
+        }
+
+
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists((string)treeView1.SelectedNode.Tag))
+            {
+                Directory.Delete((string)treeView1.SelectedNode.Tag, true);
+                treeView1.SelectedNode.Remove();
+            }
+            else
+            {
+                File.Delete((string)treeView1.SelectedNode.Tag);
+                treeView1.SelectedNode.Remove();
+            }
+
+        }
+
+        private void addfolder_Click(object sender, EventArgs e)
+        {
+            string pathstring = (string)treeView1.SelectedNode.Tag;
+            Directory.CreateDirectory(pathstring+"\\"+textBox1.Text);
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(Makenode(ppath));
+            
+        }
+    }
+}
